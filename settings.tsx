b208672c85b024aba04ee0936bbaa856071b71ee
Brr0 <risa6550@seznam.cz>
2023-12-14 20:55:27 +0100

    first commit

---
+++ settings.tsx
@@ -0,0 +1,15 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "1", mintText: "Mint price: 0.8sol", buttonLabel: "Mint now!", header: "Public mint" },
+  { label: "2", mintText: "0.6sol mint token needed", buttonLabel: "Mint now!", header: "0.6sol Mint" },
+  { label: "3", mintText: "0.5sol mint token needed", buttonLabel: "Mint now!", header: "0.5sol Mint" },
+  { label: "4", mintText: "0.4sol mint token needed", buttonLabel: "Mint now!", header: "0.4sol Mint" },
+  { label: "5", mintText: "free mint token needed", buttonLabel: "Mint now!", header: "Free mint" },
+ ];
+
+//header image in the ui. replace with your own
+export const image = "/logo.png"; 
+
+//website title
+export const headerText = "SOL Snap";
