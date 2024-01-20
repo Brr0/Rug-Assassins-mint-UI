4011c08f6506c496aadab8e09475b6804d62fb1a
Brr0 <risa6550@seznam.cz>
2024-01-20 19:06:29 +0100

    first commit

--- settings.tsx
+++ settings.tsx
@@ -1,12 +1,12 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "1", mintText: "Mint price: 105 USDC", buttonLabel: "Mint now!", header: "Public mint" },
-  { label: "2", mintText: "Mint price: 100 USDC", buttonLabel: "Mint now!", header: "WL mint" }
+  { label: "1", mintText: "Mint price: 1 SOL", buttonLabel: "Mint now!", header: "Public mint" },
+  { label: "2", mintText: "Requires a Mint token from the burn or a giveaway", buttonLabel: "Mint now!", header: "Free mint" }
  ];
 
 //header image in the ui. replace with your own
 export const image = "/logo.png"; 
 
 //website title
-export const headerText = "A1 Mining Mint";
+export const headerText = "Rug Assasins Mint";
